a27675a40f2e270cd05af02862a445ea2f873026
chr-sa <chr-sa@noreply.codeberg.org>
2023-09-29 14:21:04 +0200

    add python-application template

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     templates = rec {
       rust = { path = ./rust; description = "Rust template"; };
       python-poetry = { path = ./python-poetry; description = "Python template using poetry"; };
+      python-application = { path = ./python-application; description = "Python application template"; };
       torch = { path = ./torch; description = "Python template from nixpkgs with torch"; };
       haskell = { path = ./haskell; description = "Haskell template"; };
     };
---
+++ python-application/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def main():
+    a = np.array([1, 2, 3])
+    print(f"Hello World, {a}")
