c0cdf82f88a426f63346934005332cb9e1696471
chr-sa <chr-sa@noreply.codeberg.org>
2023-12-23 15:10:13 +0100

    add zig and trivial

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   description = "chr-sa flake templates";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self }: {
+  outputs = { self, nixpkgs }@inputs: {
     templates = rec {
+      trivial = { path = ./trivial; description = "Simple boilerplate for a flake"; };
       rust = { path = ./rust; description = "Rust template"; };
       python-poetry = { path = ./python-poetry; description = "Python template using poetry"; };
       python-application = { path = ./python-application; description = "Python application template"; };
       torch = { path = ./torch; description = "Python template from nixpkgs with torch"; };
       haskell = { path = ./haskell; description = "Haskell template"; };
+      zig = { path = ./zig; description = "Zig template"; };
     };
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
   };
 }
---
+++ trivial/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system:
+          function (system: function nixpkgs.legacyPackages.${system}));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell { packages = [ ]; };
+      });
+    };
+}
